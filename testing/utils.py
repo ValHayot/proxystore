849f4dc9d8824ea9448b3ed41369b6334e222c12
Valerie Hayot-Sasson <ValHayot@users.noreply.github.com>
2023-01-10 11:14:40 -0600

    Update utils.py

--- testing/utils.py
+++ testing/utils.py
@@ -3,24 +3,13 @@ from __future__ import annotations
 
 import socket
 
-used_ports = []
-
 def open_port() -> int: # pragma: no cover
     """Return open port.
 
     Source: https://stackoverflow.com/questions/2838244
     """
-    global used_ports
-    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 0))
     s.listen(1)
     port = s.getsockname()[1]
     s.close()
-    
-    if port in used_ports:
-        print("port already in use")
-        port = open_port()
-    else:
-        used_ports.append(port)
-    return port
