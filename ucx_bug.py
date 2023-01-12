7f0a04d395e91dc7c4caab27f9ae23fb0f9df794
Valerie Hayot-Sasson <ValHayot@users.noreply.github.com>
2023-01-12 09:18:36 -0600

    Added simple ucx code to aid in debugging error

---
+++ ucx_bug.py
@@ -0,0 +1,170 @@
+import asyncio
+import ucp
+import numpy as np
+import signal
+from time import sleep
+from multiprocessing import Process
+from random import randint
+
+host = "127.0.0.1"
+port = randint(5000, 7000)
+n_bytes = 2**30
+
+lf = None
+
+async def send(ep):
+    global lf
+    
+    # recv buffer
+    arr = np.empty(n_bytes, dtype='u1')
+    await ep.recv(arr)
+    assert np.count_nonzero(arr) == np.array(0, dtype=np.int64)
+    print("Received NumPy array")
+
+    # increment array and send back
+    arr += 1
+    print("Sending incremented NumPy array")
+    await ep.send(arr)
+
+    print("Endpoint closed")
+    await ep.close()
+
+def close_listener(*args):
+    global lf
+    lf.close()
+    print('closing')
+    assert lf.closed()
+
+def launch_server():
+    asyncio.run(server_main())
+
+async def server_main():
+    global lf
+    lf = ucp.create_listener(send, port)
+
+    # Set the stop condition when receiving SIGINT (ctrl-C) and SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    await stop
+    close()
+    await reset_ucp_async()
+
+def reset_ucp() -> None:  # pragma: no cover
+    """Hard reset all of UCP.
+
+    UCP provides :code:`ucp.reset()`; however, this function does not correctly
+    shutdown all asyncio tasks and readers. This function wraps
+    :code:`ucp.reset()` and additionally removes all readers on the event loop
+    and cancels/awaits all asyncio tasks.
+    """
+
+    def inner_context() -> None:
+        ctx = ucp.core._get_ctx()
+
+        for task in ctx.progress_tasks:
+            if task is None:
+                continue
+            task.event_loop.remove_reader(ctx.epoll_fd)
+            if task.asyncio_task is not None:
+                try:
+                    task.asyncio_task.cancel()
+                    task.event_loop.run_until_complete(task.asyncio_task)
+                except asyncio.CancelledError:
+                    pass
+
+    # We access ucp.core._get_ctx() inside this nested function so our local
+    # reference to the UCP context goes out of scope before calling
+    # ucp.reset(). ucp.reset() will fail if there are any weak references to
+    # to the UCP context because it assumes those may be Listeners or
+    # Endpoints that were not properly closed.
+    inner_context()
+
+    try:
+        ucp.reset()
+    except ucp.UCXError:
+        pass
+
+async def reset_ucp_async() -> None:  # pragma: no cover
+    """Hard reset all of UCP.
+
+    UCP provides :code:`ucp.reset()`; however, this function does not correctly
+    shutdown all asyncio tasks and readers. This function wraps
+    :code:`ucp.reset()` and additionally removes all readers on the event loop
+    and cancels/awaits all asyncio tasks.
+    """
+
+    async def inner_context() -> None:
+        ctx = ucp.core._get_ctx()
+
+        for task in ctx.progress_tasks:
+            if task is None:
+                continue
+            task.event_loop.remove_reader(ctx.epoll_fd)
+            if task.asyncio_task is not None:
+                try:
+                    task.asyncio_task.cancel()
+                    await task.asyncio_task
+                except asyncio.CancelledError:
+                    pass
+
+    # We access ucp.core._get_ctx() inside this nested function so our local
+    # reference to the UCP context goes out of scope before calling
+    # ucp.reset(). ucp.reset() will fail if there are any weak references to
+    # to the UCP context because it assumes those may be Listeners or
+    # Endpoints that were not properly closed.
+    await inner_context()
+
+    try:
+        ucp.reset()
+    except ucp.UCXError:
+        pass
+
+def close() -> None:
+    global lf
+    if lf is not None:
+        lf.close()
+
+        while not lf.closed():
+            sleep(0.001)
+
+        # Need to lose reference to Listener because UCP does reference
+        # counting
+        del lf
+        lf = None
+
+def server_start():
+    asyncio.run(server_main())
+
+async def client_main(proc):
+    iterations = randint(1, 9)
+    msg = np.zeros(n_bytes, dtype='u1') # create some data to send
+
+    for i in range(iterations):
+        ep = await ucp.create_endpoint(host, port)
+        # send message
+        print("Send Original NumPy array")
+        await ep.send(msg)  # send the real message
+
+        # recv response
+        print("Receive Incremented NumPy arrays")
+        resp = np.empty_like(msg)
+        await ep.recv(resp)  # receive the echo
+        np.testing.assert_array_equal(msg + 1, resp)
+
+    await ep.close()
+    assert ep.closed()
+    proc.terminate()
+
+if __name__ == "__main__":
+
+    p = Process(target=server_start)
+    p.start()
+    sleep(5)
+    try:
+        _loop = asyncio.get_running_loop()
+    except RuntimeError:
+        _loop = asyncio.new_event_loop()
+    _loop.run_until_complete(client_main(p))
